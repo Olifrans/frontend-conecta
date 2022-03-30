55fac4afc3e24770d37e3379562e7ce964ce4452
FRANCISCO OLIVEIRA <fransoliveira@gmail.com>
2022-03-29 21:26:13 -0300

    Olifrans Dev React Net Core

---
+++ src/pages/Home/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../../Home/style.css'
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="toolbar">
+        <div>
+          <span>Direita -Olifrans Dev React Net Core</span>
+          <a href="/"> Dev React Net Core</a>
+        </div>
+        <div>
+          <button>Novo Post</button>
+          <span>Imagem01</span>
+          <span>Imagem02</span>
+        </div>
+      </div>
+    </header>
+  )
+}
